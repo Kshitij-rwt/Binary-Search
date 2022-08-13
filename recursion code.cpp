c0af558aa55cf077480b78803e08f6220a1f92c8
Kshitij-rwt <110712175+Kshitij-rwt@users.noreply.github.com>
2022-08-13 21:30:33 +0530

    Add files via upload

---
+++ recursion code.cpp
@@ -0,0 +1,40 @@
+// C++ program to implement recursive Binary Search
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
+
+		if (arr[mid] == x)
+			return mid;
+		if (arr[mid] > x)
+			return binarySearch(arr, l, mid - 1, x);
+
+		return binarySearch(arr, mid + 1, r, x);
+	}
+
+	return -1;
+}
+
+int main(void)
+{
+    int m;
+    int x;
+    cout<<"enter no. of elements";
+    cin>>m;
+	int arr[m] ;
+	cout<<"enter the elements";
+	for(int i=0;i<m;i++)
+        cin>>arr[i];
+    cout<<"enter element to search for";
+    cin>>x;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = binarySearch(arr, 0, n - 1, x);
+	(result == -1)
+		? cout << "Element is not present in array"
+		: cout << "Element is present at index " << result;
+	return 0;
+}
+
